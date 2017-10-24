6138794c0b8744609c538e242a671a6eaa2b6135
Nicolas Vanvyve <nicolas.vanvyve@student.uclouvain.be>
2017-10-24 11:34:46 +0200

    interface

---
+++ promotions/exercises.py
@@ -0,0 +1,11 @@
+#INTERFACE
+class exercises
+    def __init__(self, exercise, error = None):
+        self.exercise = exercise
+        self.error = error
+
+    def isEquivalant(self, other):
+        pass
+
+    def isSolution(self, solution):
+        pass
---
+++ promotions/exercises_factory.py
@@ -0,0 +1,5 @@
+class exercises_factory
+    def createEx(self,type,parseInput,ed):
+        self.type = type
+        self.parseInput = parseInput
+        self.ed = ed
