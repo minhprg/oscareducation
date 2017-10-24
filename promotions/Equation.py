5d767b8e669ad683a0566ced09259674ee3c096e
artbil <arthur.billion@student.uclouvain.be>
2017-10-24 12:44:02 +0200

    equations

--- promotions/Equation.py
+++ promotions/Equation.py
@@ -6,16 +6,20 @@ from sympy import Symbol
 class Equation: #(Exercice):
     def __init__(self, equation, error = None):
         self.equation = equation
+        x = Symbol('x')
+        self.solution = solve(self.equation,x)
         self.error = error
 
     def isEquivalant(self, other):
-        pass
-
-    def isSolution(self, solution):
         x = Symbol('x')
-        return solution == solve(self.equation,x)[0]
+        return solve(other.equation,x) == self.solution
+
+    def isSolution(self, solution): #int solution
+        return solution == self.solution[0]
 
 
 #tests
-Equation = Equation('x+2-(1+2)')
-print Equation.isSolution(1)
+#Equation1 = Equation('x+2-(1+2)')
+#print Equation1.isSolution(1)
+#Equation2 = Equation('x-(1)')
+#print Equation1.isEquivalant(Equation2)
