efc8fcfde8927ac5951b79751b6c46b1b8a97c90
artbil <arthur.billion@student.uclouvain.be>
2017-10-24 12:49:34 +0200

    equations

--- promotions/Equation.py
+++ promotions/Equation.py
@@ -4,8 +4,9 @@ from sympy.solvers import solve
 from sympy import Symbol
 
 class Equation: #(Exercice):
-    def __init__(self, equation, error = None):
+    def __init__(self, equation, lettre, error = None):
         self.equation = equation
+        self.lettre = lettre
         x = Symbol('x')
         self.solution = solve(self.equation,x)
         self.error = error
@@ -19,7 +20,7 @@ class Equation: #(Exercice):
 
 
 #tests
-#Equation1 = Equation('x+2-(1+2)')
+#Equation1 = Equation('x+2-(1+2)','a')
 #print Equation1.isSolution(1)
-#Equation2 = Equation('x-(1)')
+#Equation2 = Equation('x-(1)','y')
 #print Equation1.isEquivalant(Equation2)
