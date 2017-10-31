cc4d8e9169a4c25165f4b1955cb377b22d7a4712
algerlache <alexandre.gerlache@student.uclouvain.be>
2017-10-31 17:17:55 +0100

    ineq parser

--- promotions/InputHandler.py
+++ promotions/InputHandler.py
@@ -1,10 +1,19 @@
 
 
 class InputHandler:
-    def __init__(self): #type = 0, 1, 2, 3, 4, 5
-        pass
+    def __init__(self, type): #type = 0, 1, 2, 3, 4, 5
+        self.type = type
+    def parse(self, inputString):
+        if self.type == "algebraicEquation":
+            return self.parseEq(inputString)
+        elif self.type == "algebraicInequation":
+            return self.parseIneq(inputString)
+        elif self.type == "algebraicSystem":
+            return self.parseSys(inputString)
+        else:
+            return "Type not found"
 
-    def parse(self,inputString):
+    def parseEq(self,inputString):
         inputString = inputString.replace(" ", "")
         s = inputString.split("=")
         if len(s) == 2:
@@ -26,12 +35,40 @@ class InputHandler:
         else:
             bool1 = False
             bool2 = False
-        if bool1 and bool2:
+        variables = self.findVariables(inputString)
+        if bool1 and bool2 and len(variables)==1:
             inputString = s[0]+"-("+s[1]+")"
             inputString = inputString.replace("^", "**")
-            return inputString #appler exercice avec inputString correctement parser
+            return (inputString, variables[0]) #appler exercice avec inputString correctement parser
+        elif len(variables) > 1 and bool1 and bool2:
+            return "Trop de variables"
         else:
-            return "L'equation n'est pas bien exprimee"#print parse error
+            return "L'equation n'est pas bien exprimee" #print parse error
+
+    def findVariables(self, inputString):
+        variables = []
+        for elem in list(inputString):
+            if elem.isalpha() and elem not in variables:
+                variables.append(elem)
+        print variables
+        return variables
+
+    def parseIneq(self,inputString):
+        inputString = inputString.replace(" ", "")
+        listChar = list(inputString)
+        if listChar[0].isalnum() or listChar[0] == "(" or listChar[0] == "-":
+            bool = self.checkParseErrorIneq(listChar)
+        else:
+            bool = False
+
+        variables = self.findVariables(inputString)
+        if bool and len(variables)==1:
+            inputString = inputString.replace("^", "**")
+            return (inputString, variables[0]) #appler exercice avec inputString correctement parser
+        elif (len(variables) > 1 or len(variables) == 0) and bool:
+            return "Trop ou pas de variable(s)"
+        else:
+            return "L'inequation n'est pas bien exprimee" #print parse error
 
     def checkParseError(self,listChar, nbPar = 0):
         if not listChar and nbPar == 0:
@@ -94,3 +131,91 @@ class InputHandler:
             else:
                 return False
 
+    def checkParseErrorIneq(self,listChar, nbPar = 0):
+        if not listChar and nbPar == 0:
+            return True #no parse error
+
+        if nbPar < 0:
+            return False
+        if len(listChar) > 1:
+            if listChar[0].isnumeric():
+                if listChar[1].isalnum():
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar)
+                elif listChar[1] == "+" or listChar[1] == "-" or listChar[1] == "*" or listChar[1] == "/" or listChar[1] == "^":
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar)
+                elif listChar[1] == ")":
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar)
+                elif (listChar[1] == "<" or listChar[1] == ">") and nbPar == 0:
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar)
+                else:
+                    return False
+            elif listChar[0].isalpha():
+                if listChar[1] == "+" or listChar[1] == "-" or listChar[1] == "*" or listChar[1] == "/" or listChar[1] == "^":
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar)
+                elif listChar[1] == ")":
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar)
+                elif (listChar[1] == "<" or listChar[1] == ">") and nbPar == 0:
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar)
+                else:
+                    return False
+            elif listChar[0] == "+" or listChar[0] == "-" or listChar[0] == "*"  or listChar[0] == "/" or listChar[0] == "^":
+                if listChar[1].isalnum():
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar)
+                elif listChar[1] == "(":
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar,nbPar)
+                else:
+                    return False
+            elif listChar[0] == "(":
+                if listChar[1].isalnum() or listChar[1] == "(" or listChar[1] == "-":
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar+1)
+                else:
+                    return False
+            elif listChar[0] == ")":
+                if listChar[1] == "+" or listChar[1] == "-" or listChar[1] == "*" or listChar[1] == "/" or listChar[1] == "<" or listChar[1] == ">" or listChar[1] == "^" or listChar[1] == ")":
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar,nbPar-1)
+                else:
+                    return False
+            elif listChar[0] == "<" or listChar[0] == ">":
+                if listChar[1].isalnum():
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar,nbPar)
+                elif listChar[1] == "=" or listChar[1] == "-" or listChar[1] == "(":
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar)
+                else:
+                    return False
+            elif listChar[0] == "=":
+                if listChar[1].isalnum():
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar,nbPar)
+                elif listChar[1] == "-" or listChar[1] == "(":
+                    del listChar[0]
+                    return self.checkParseErrorIneq(listChar, nbPar)
+                else:
+                    return False
+            else:
+                return False
+        elif len(listChar) == 0 and nbPar == 0:
+            return True
+        else:
+            if nbPar == 1 and listChar[0] == ")":
+                return True
+            elif listChar[0].isalnum() and nbPar == 0:
+                return True
+            else:
+                return False
+
+handler = InputHandler("algebraicInequation")
+parsed = handler.parse(u"(2*a-3)>=-5")
+print parsed
