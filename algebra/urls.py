4c332495bfdbbe9288e76f6c07aab864c4bb840c
Thomas <thomas.beckers@student.uclouvain.be>
2017-11-03 13:51:36 +0100

    Add TrainingSession in import

--- algebra/urls.py
+++ algebra/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from algebra.views import List, ExerciceCreation, AssessmentCreation
+from algebra.views import List, ExerciceCreation, AssessmentCreation, TrainingSession
 
 urlpatterns = [
     url(r'^$', List.as_view(), name='list'),
     url(r'^exercice/creation$', ExerciceCreation.as_view(), name='exercice_creation'),
     url(r'^assessment/creation$', AssessmentCreation.as_view(), name='assessment_creation'),
-    url(r'^student/training_session$', TrainingSession.as_view(), name='training_session'),
+    url(r'^student/training_session$', TrainingSession.as_view(), name='training_session_student'),
 ]
