a611354f55eac21f5aeff31599a67ebaa6ee7e95
Valentin Haveaux <32429241+ValentinHaveauxUCL@users.noreply.github.com>
2017-11-07 15:14:25 +0100

    modif view.py
    tab training session

--- algebra/views.py
+++ algebra/views.py
@@ -17,12 +17,11 @@ class List(View):
 
 	def get(self, request):
 		return TemplateResponse(request, "algebra/list.haml")
-    
+
 class TrainingSession(View):
 
 	def get(self, request):
-        #return TemplateResponse(request, "algebra/training_session_student_html.html")
-        return TemplateResponse(request, "algebra/training_session_student.haml")
+		return TemplateResponse(request, "algebra/training_session_student.haml")
 
 
 class ExerciceCreation(View):
