2949c268ecbfcbb640f3c013cb9ebb45550798c6
sponchau <simon.ponchau@student.uclouvain.be>
2017-11-15 18:56:02 +0100

    Generator

--- algebra/engine/Generator.py
+++ algebra/engine/Generator.py
@@ -13,15 +13,56 @@ class Generator(object):
     _operators = ['-', '+', '*', '/']
 
     @staticmethod
-    def generate(degree=2, range=20, domain=S.Reals):
-        pass
+    def generate(degree=2, ranges=20, domain=S.Reals):
+        number = choice(range(0, 10))
+
+        if 0<=number<=5:
+            return Generator.generateEquation(degree,ranges)
+        elif number<=7:
+            return Generator.remarkableProducts(ranges)
+        elif number<=9:
+            return Generator.remarkableProductsSum(ranges)
+        else:
+            return Generator.impossibleEquation(ranges)
+
 
     @staticmethod
     def solution(e_degree, e_range):
         solutions = []
 
-        solutions.append(choice(range(-e_range, e_range)))
-        if e_degree is 2:
+        for i in range (0,e_degree):
             solutions.append(choice(range(-e_range, e_range)))
 
         return tuple(solutions)
+
+    @staticmethod
+    def generateEquation(e_degree, e_range):
+        tabSolution = Generator.solution(e_degree, e_range)
+
+        string = ""
+        for i in range (0,e_degree):
+            if i!=0 :
+                string+="*"
+            string += "(x-"+str(tabSolution[i])+")"
+
+        return string
+
+    @staticmethod
+    def remarkableProducts(e_range):
+        tabSolution = Generator.solution(2, e_range)
+        string = "("+str(tabSolution[0])+"*x+"+str(tabSolution[1])+")^2"
+        return string
+
+    @staticmethod
+    def remarkableProductsSum(e_range):
+        tabSolution = Generator.solution(2, e_range)
+        string = "("+str(tabSolution[0])+"*x)^2-("+str(tabSolution[1])+")^2"
+        return string
+
+    @staticmethod
+    def impossibleEquation(e_range):
+        delta=0
+        while delta>=0:
+            tabSolution = Generator.solution(3, e_range)
+            delta = tabSolution[1]**2-4*tabSolution[0]*tabSolution[2]
+        return str(tabSolution[0])+"*x^2+"+str(tabSolution[1])+"*x+"+str(tabSolution[2])
