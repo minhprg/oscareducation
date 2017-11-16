13963d5707b764560d200c140c4e27bda1f11264
Towelythetowel <vincent.vandervilt@student.uclouvain.be>
2017-11-16 18:22:48 +0100

    inéquations et systèmes presques réparés

--- student/static/EquationForm.js
+++ student/static/EquationForm.js
@@ -2,14 +2,13 @@ var myDic = {};
 function submit2(id){
     if( !(id in myDic)){
        myDic[id] = [];
-       //myDic[id].push(document.getElementById("ans".concat(id.toString())).value);
     }
     var text = document.getElementById("text".concat(id.toString())).value;
     if(text != ""){
         myDic[id].push(text);
+        listToAnswer(id);
+        document.getElementById("text".concat(id.toString())).value = "";
     }
-    listToAnswer(id);
-    document.getElementById("ans".concat(id.toString())).value = "";
 
 
 }
@@ -29,15 +28,15 @@ function clearLastStep(id){
        }
     }
 }
-function pushFirstEquation(id,equation){
-    if( !(id in myDic)){
-       myDic[id] = [];
+function clearLastStepSystem(id){
+    if( id in myDic){
+       if(myDic[id].length != 0) {
+           myDic[id].pop();
+           listToAnswerSystem(id);
+       }
     }
-    myDic[id].push(equation);
-    document.getElementById("pre".concat(id.toString())).innerHTML = myDic[id];
-    document.getElementById(id.toString()).innerHTML = myDic[id];
-
 }
+
 function myInputHandler(event, id){
     	if(event.which == 13 | event.keyCode == 13) {
             event.preventDefault();
@@ -46,4 +45,38 @@ function myInputHandler(event, id){
             return false;
         }
         return true;
+}
+function myInputHandlerSystem(event, id){
+    	if(event.which == 13 | event.keyCode == 13) {
+            event.preventDefault();
+            event.stopImmediatePropagation();
+            submitSystem(id);
+            return false;
+        }
+        return true;
+}
+function submitSystem(id){
+    if( !(id in myDic)){
+       myDic[id] = [];
+       //myDic[id].push(document.getElementById("ans".concat(id.toString())).value);
+    }
+    var texteq1 = document.getElementById("textEq1".concat(id.toString())).value;
+    var texteq2 = document.getElementById("textEq2".concat(id.toString())).value;
+    console.log(texteq1)
+    console.log(texteq2)
+    if(texteq1 != "" && texteq2 != ""){
+        myDic[id].push([texteq1, texteq2]);
+        listToAnswerSystem(id);
+        document.getElementById("textEq2".concat(id.toString())).value = "";
+        document.getElementById("textEq1".concat(id.toString())).value = "";
+    }
+}
+
+function listToAnswerSystem(id){
+    document.getElementById("pre".concat(id.toString())).innerHTML = myDic[id];
+    var txt = ""
+    for (var i = 0 ; i < myDic[id].length; i++){
+        txt=txt.concat(":").concat(myDic[id][i][0].concat(";").concat(myDic[id][i][1]));
+    }
+    document.getElementById(id.toString()).value = txt;
 }
