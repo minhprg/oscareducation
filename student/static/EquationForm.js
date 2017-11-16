78f161abe9227778f7d8f6350cec088433328502
Towelythetowel <vincent.vandervilt@student.uclouvain.be>
2017-11-16 11:53:20 +0100

    answer submission working

--- student/static/EquationForm.js
+++ student/static/EquationForm.js
@@ -1,27 +1,33 @@
 var myDic = {};
 function submit2(id){
-    console.log(id);
-    console.log(document.getElementById("ans".concat(id.toString())).value)
-   console.log(document.getElementById("text".concat(id.toString())).value)
     if( !(id in myDic)){
        myDic[id] = [];
-       myDic[id].push(document.getElementById("ans".concat(id.toString())).value);
+       //myDic[id].push(document.getElementById("ans".concat(id.toString())).value);
     }
     var text = document.getElementById("text".concat(id.toString())).value;
     if(text != ""){
         myDic[id].push(text);
     }
+    listToAnswer(id);
+    document.getElementById("ans".concat(id.toString())).value = "";
 
+
+}
+function listToAnswer(id){
     document.getElementById("pre".concat(id.toString())).innerHTML = myDic[id];
     var txt = ""
     for (var i = 0 ; i < myDic[id].length; i++){
         txt=txt.concat(":").concat(myDic[id][i]);
     }
-    console.log(txt);
     document.getElementById(id.toString()).value = txt;
-    console.log(document.getElementById(id.toString()).value);
-
-
+}
+function clearLastStep(id){
+    if( id in myDic){
+       if(myDic[id].length != 0) {
+           myDic[id].pop();
+           listToAnswer(id);
+       }
+    }
 }
 function pushFirstEquation(id,equation){
     if( !(id in myDic)){
@@ -32,4 +38,12 @@ function pushFirstEquation(id,equation){
     document.getElementById(id.toString()).innerHTML = myDic[id];
 
 }
-
+function myInputHandler(event, id){
+    	if(event.which == 13 | event.keyCode == 13) {
+            event.preventDefault();
+            event.stopImmediatePropagation();
+            submit2(id);
+            return false;
+        }
+        return true;
+}
